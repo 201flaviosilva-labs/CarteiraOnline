03b9e676146b9855e6ba46e18eb2a3edba416d0d
flaviosilva <flavioandre.2001.9.7@gmail.com>
2020-06-05 21:01:13 +0100

    Pagina Home e imagens

--- Data/ConexaoTemplate.php
+++ Data/ConexaoTemplate.php
@@ -2,7 +2,7 @@
 session_start();
 $servername = "localhost";
 $username = "root";
-$password = "Beep";
+$password = "";
 $dbname = "CarteiraOnline";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
